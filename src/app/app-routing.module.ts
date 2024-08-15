d314a8ef5907890b007cd099f1f09fbb657eead1
Estebandido123 <este.toledo@duocuc.cl>
2024-08-15 16:41:32 -0400

    habitaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'habitaciones',
+    loadChildren: () => import('./pages/habitaciones/habitaciones.module').then( m => m.HabitacionesPageModule)
+  },
 
 ];
 

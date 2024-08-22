7d78306a980212e8e9a6d212f989f929436e04bc
Estebandido123 <este.toledo@duocuc.cl>
2024-08-22 17:22:44 -0400

    mejoras

--- src/app/pages/hfamiliar/hfamiliar.page.ts
+++ src/app/pages/hfamiliar/hfamiliar.page.ts
@@ -7,56 +7,13 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./hfamiliar.page.scss'],
 })
 export class HfamiliarPage implements OnInit {
-  public alertButtons = [
-    {
-      text: 'Cancel',
-      role: 'cancel',
-      handler: () => {
-        console.log('Alert canceled');
-      },
-    },
-    {
-      text: 'OK',
-      role: 'confirm',
-      handler: () => {
-        console.log('Alert confirmed');
-      },
-    },
-  ];
 
-  public alertInputs = [
-    {
-      name: 'camas',
-      type: 'text',
-      placeholder: 'Camas',
-      value: 'Camas: 2',
-      attributes: { readonly: true },
-    },
-    {
-      name: 'banos',
-      type: 'text', 
-      placeholder: 'Baños',
-      value: 'Baños: 2',
-      attributes: { readonly: true },
-    },
-  ];
-
+  isModalOpen = false;
   constructor(private alertController: AlertController) {}
-
+  
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
   ngOnInit() {}
 
-  async mostrarInfo(info: string) {
-    const alert = await this.alertController.create({
-      header: 'Información de la Habitación',
-      message: 'Aquí puedes ver los detalles sobre la habitación seleccionada.',
-      inputs: this.alertInputs.map(input => ({
-        ...input,
-        type: input.type as 'text' | 'textarea' | 'password' | 'number' | 'email', 
-      })),
-      buttons: this.alertButtons,
-      cssClass: 'alerta',
-    });
-
-    await alert.present();
-  }
 }

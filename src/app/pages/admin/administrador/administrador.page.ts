932c727748eb703d4fda5bdbc92da58b9d99028a
andyMP2004 <a.madrid@duocuc.cl>
2024-08-27 17:30:14 -0400

    listas

--- src/app/pages/admin/administrador/administrador.page.ts
+++ src/app/pages/admin/administrador/administrador.page.ts
@@ -8,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
 export class AdministradorPage implements OnInit {
 
   seleccion: string ="";
+  reservas = [
+    {habitacion: 'Suite Familiar', fecha: '2024-08-25', hora: '14:00' },
+    {habitacion: 'Suite Presidencial', fecha: '2024-08-26', hora: '16:00' },
+    {habitacion: 'Suite', fecha: '2024-08-27', hora: '10:00' }
+  ];
+
+  habitaciones = [
+    {habitacion:'Habitacion Familiar', numero: 15, id:1},
+    {habitacion:'Suite de lujo', numero: 30, id:12},
+    {habitacion:'Suite precidencial', numero: 50, id:30}
+
+  ];
+
+  usuarios = [
+    {usu:'andy madrid',rut:'21687221-5',id:'1'},
+    {usu:'esteban toledo',rut:'21836167-2',id:'2'},
+    {usu:'aaron vazques',rut:'22432672-2',id:'3'},
+    {usu:'basthian bascuñan ',rut:'22932723-4',id:'4'}
+    
+  ]
 
   constructor() { }
 

e4895d0554fb62f10b212113bb2412d652b2e2c3
Estebandido123 <este.toledo@duocuc.cl>
2024-08-27 22:38:43 -0400

    q

--- src/app/pages/ayuda/ayuda.page.ts
+++ src/app/pages/ayuda/ayuda.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ayuda',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AyudaPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu:MenuController) { }
 
-  ngOnInit() {
+  ngOnInit() {this.menu.enable(false);
   }
 
 }

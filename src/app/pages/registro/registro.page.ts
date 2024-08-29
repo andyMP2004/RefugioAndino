9efc43438119f0ce3033536567531b68d24c0327
andyMP2004 <a.madrid@duocuc.cl>
2024-08-29 17:41:31 -0400

    length contraseña

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -43,6 +43,13 @@ export class RegistroPage implements OnInit {
         buttons: ['Aceptar'],
       });
       await alert.present();
+    }else if (this.contrasena.length<=6 || this.contrasena.length>=12){
+      const alert = await this.alertController.create({
+        header: 'contraseña invalida',
+        message: 'la contraseña debe tener entre 6 y 12 caracteres',
+        buttons: ['Aceptar'],
+      });
+      await alert.present();
     }else {
       let navigationExtras: NavigationExtras = {
         state: {

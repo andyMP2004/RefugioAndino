9ecb99f41c1555b44fbfd4108b4e73b9d8f81791
Estebandido123 <este.toledo@duocuc.cl>
2024-09-03 19:18:46 -0400

    listoco

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -11,32 +11,38 @@ export class AgregarPage implements OnInit {
   camas: string ="";
   banos: string ="";
   constructor(private menu: MenuController, private router: Router, private alertController: AlertController) { }
-
-
-  async agregar(){
-    if (!this.banos || !this.camas) {
+  async agregar() {
+    if(!this.camas || !this.banos){
       const alert = await this.alertController.create({
-        header: 'Los datos no pueden estar vacíos',
+        header: 'Los datos no pueden estar vacios',
         message: 'Por favor, complete todos los datos',
-        buttons: ['añadir'],
-      });
-      await alert.present();
-  }else {
-    const alert = await this.alertController.create({
-      header: 'habitacion agregada',
-      buttons: ['añadir'],
-    });
-    await alert.present();
-  }
-    
-    let navigationExtras: NavigationExtras = {
-      state: {
-        cam: this.camas,
-        ban: this.banos,
-      }
+        buttons: ['Aceptar'],
+      })
+      await alert.present()
+    }else if(this.camas.length<5 || this.camas.length>50){
+      const alert = await this.alertController.create({
+        header: 'La descripcion debe tener entre 6 y 50 caracteres',
+        message: 'Por favor, intente nuevamente',
+        buttons: ['Aceptar'],
+      })
+      await alert.present()
+    }else if(this.banos.length<5 || this.banos.length>50){
+      const alert = await this.alertController.create({
+        header: 'La descripcion debe tener entre 6 y 50 caracteres',
+        message: 'Por favor, intente nuevamente',
+        buttons: ['Aceptar'],
+      })
+      await alert.present()
+    }else{
+        let navigationExtras: NavigationExtras = {
+        state: {
+          cam: this.camas,
+          ban: this.banos,        }
+      };
+      this.router.navigate(['/hfamiliar'], navigationExtras);
     }
-    this.router.navigate(['/hfamiliar'], navigationExtras) ;
-}
+
+  }
   ngOnInit() {this.menu.enable(false);
   }
 }

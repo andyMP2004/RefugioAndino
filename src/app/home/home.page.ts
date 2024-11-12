89a0b3d1493f5867327b8da376eb5b57d1bd6479
andyMP2004 <a.madrid@duocuc.cl>
2024-11-11 22:25:43 -0300

    nt

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -26,7 +26,7 @@ export class HomePage {
 
 
   async irPagina() {
-    if (this.correo === "" && this.contrasena === "") { 
+    if (this.correo === "" || this.contrasena === "") { 
       const alert = await this.alertController.create({
         header: 'Campos Vacíos',
         message: 'Por favor intente de nuevo',
@@ -35,36 +35,73 @@ export class HomePage {
       await alert.present();
     } else {
 
-      if (this.correo === "admin@gmail.com" && this.contrasena === "Admin123") { 
+    if (this.correo === "admin@gmail.com" && this.contrasena === "Admin123") { 
         this.router.navigate(['/administrador']);
         return;
-      }
-  
-      let ValidarUsuario = await this.authService.inicioSesion(this.correo, this.contrasena);
-      let Validarcorreo = await this.bd.BuscarUsuC(this.correo);
-  
-      if (Validarcorreo && Validarcorreo.estadoidestado === 2) {
-        const alert = await this.alertController.create({
-          header: 'Usuario Desactivado',
-          message: 'Su cuenta está desactivada',
-          buttons: ['OK'],
-        });
-        await alert.present();
-        return;
-      } else if (ValidarUsuario) {
-        await this.bd.modificarContra(this.correo, Validarcorreo.idusuario);
-        // Guardar los datos del usuario en el NativeStorage
-        await this.storage.setItem('usuario', Validarcorreo.idusuario);
-        this.router.navigate(['/habitaciones']);
-      } else {
-        const alert = await this.alertController.create({
-          header: 'Inicio de Sesión Fallido',
-          message: 'Correo o contraseña incorrectos.',
-          buttons: ['OK'],
-        });
-        await alert.present();
-      }
     }
+    try {
+          let CredencialFireBase = await this.authService.inicioSesion(this.correo, this.contrasena);
+          
+          if (CredencialFireBase) {
+
+            // Verificamos el usuario en la base de datos
+            let ValidarUsuario = await this.bd.BuscarUsuC(this.correo);
+    
+            if (ValidarUsuario) {
+
+              if (ValidarUsuario.estadoidestado == '2') {
+                const alert = await this.alertController.create({
+                    header: 'Usuario Desactivado',
+                    message: 'Contacta con soporte',
+                    buttons: ['OK'],
+                    cssClass: 'estilo-alertas'
+                });
+                await alert.present();
+                return;
+              }
+
+              await this.bd.modificarContra(this.contrasena, ValidarUsuario.idusuario);
+
+              // Guardar los datos del usuario en el NativeStorage
+              await this.storage.setItem('usuario', ValidarUsuario.idusuario);
+
+              /* if (ValidarUsuario.id_rol_fk == "1") {
+                this.router.navigate(['/homeadmin']);
+              }else{
+                // Redirigir al home */
+                this.router.navigate(['/habitaciones']);
+              /* } */
+              
+            } else { 
+              const alert = await this.alertController.create({
+                header: 'Error al iniciar sesión',
+                message: 'Usuario o contraseña incorrectos, por favor intente de nuevo.',
+                buttons: ['OK'],
+                cssClass: 'estilo-alertas'
+              });
+              await alert.present();
+            }
+          } else{
+            const alert = await this.alertController.create({
+              header: 'Error al iniciar sesión',
+              message: 'Usuario o contraseña incorrectos, por favor intente de nuevo.',
+              buttons: ['OK'],
+              cssClass: 'estilo-alertas'
+            });
+            await alert.present();
+          }
+        } catch (error) {
+
+          // Si ocurre algún error (en Firebase o en la base de datos)
+          const alert = await this.alertController.create({
+            header: 'Error al iniciar sesión',
+            message: 'Usuario o contraseña incorrectos, por favor intente de nuevo.',
+            buttons: ['OK'],
+            cssClass: 'estilo-alertas'
+          });
+          await alert.present();
+        }
+      }
   }
   
 
